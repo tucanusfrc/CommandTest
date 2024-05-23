1d09063367d12674ddf3810b8af8c60946bd9ca1
tucanusfrc <tucanusnffrc@gmail.com>
2024-05-23 20:08:09 -0300

    commit test

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+public class Constants {
+    public static class OperatorConstants{
+        public static final int driveSpeed = 1;
+        public static final int driveControllPort = 0;
+
+        public static final boolean motor = true;
+    }
+}
